8c95feeac833551bf90c49c0119a3fee1e9843d7
Chetonix <chetonix@gmail.com>
2022-11-18 17:46:59 +0530

    add birthday reminder app to the repo

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState }  from 'react';
 import data from './data';
 import List from './List';
+
+
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [people, setPeople] = useState(data);
+
+  const dataList = people.map(item => <List key={item.id} {...item}/>)
+  const dataLength = people.length;
+
+  return (
+  <main>
+    <section className='container'>
+      <h3>{dataLength} birthdays today</h3>
+      {dataList}
+      <button onClick={() => setPeople([])}>clear all</button>
+    </section>
+  </main>
+  )
 }
 
 export default App;
