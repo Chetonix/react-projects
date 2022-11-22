2f5bd4acf19f170e233f859454f252c9bb4d28a5
Chetonix <chetonix@gmail.com>
2022-11-22 10:20:43 +0530

    add some initial code to the repo

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -4,8 +4,49 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const getData = async () => {
+    try{
+      setIsLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setTours(data);
+      setIsLoading(false);
+    }catch(error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(()=> {
+    getData();
+  }, []);
+
+  if(isLoading) {
+      return <Loading />
+  }
+  
+  if(tours.length === 0) {
+    return (
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={()=> getData()}>refresh</button>
+      </div>
+    )
+  }
+
+  return (
+    <Tours tours={tours} removeTour={removeTour}/>
+  )
+
+    
 }
 
 export default App
