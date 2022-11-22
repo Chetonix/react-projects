3bf5c3defc06a155f2754dc8a756e226768141dd
Chetonix <chetonix@gmail.com>
2022-11-22 13:01:36 +0530

    add some code to the 04-accordion setup folder

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className='info'>
+        <SingleQuestion />
+      </section>
+    </div>
+  );
 }
 
 export default App;
