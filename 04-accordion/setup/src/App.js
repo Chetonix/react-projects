29ac241e97ff4b3de35897ce2a2f497f7778b0aa
Chetonix <chetonix@gmail.com>
2022-11-22 14:20:28 +0530

    add myversion of the code in 04-accordion, complete

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
+
+  const [questions, setQuestions] = useState(data);
+
   return (
     <div className="container">
       <h3>questions and answers about login</h3>
       <section className='info'>
-        <SingleQuestion />
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />
+        })}
       </section>
     </div>
   );
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
+const Question = ({id, title, info}) => {
+
+  const [expandData, setExpandData] = useState(false);
+
   return (
     <article className='question'>
       <header>
-        <h4>{}</h4>
-        <button className="btn">
-          
+        <h4>{title}</h4>
+        <button className="btn" onClick={()=>setExpandData(!expandData)}>
+          {expandData ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
+      {expandData && <p>{info}</p>}
     </article>
   )
 };
