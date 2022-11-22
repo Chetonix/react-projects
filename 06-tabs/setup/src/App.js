a18a95145f2e5ce6d8957a983266e83fc0cbe4bd
Chetonix <chetonix@gmail.com>
2022-11-22 17:09:12 +0530

    add some basic structure code for the 06-tabs project

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,51 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [roles, setRoles ] = useState([]);
+  const getData = async () => {
+
+    setIsLoading(true);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    setRoles(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if(isLoading) {
+    return(
+    <section className="section loading">
+        <h1>Loading...</h1>
+    </section>
+    )
+  }
+
+  return (
+    <section className="section">
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className="jobs-center">
+        <div className='btn-container'>
+        {roles.map(role => {
+          return (
+            <button className='job-btn' key={role.id}>{role.company}</button>
+          )
+        })}
+      </div>
+      <article className='job-info'>
+        <h3></h3>
+      </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
