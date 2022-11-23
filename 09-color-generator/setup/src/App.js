431e937b8130ada08cc4ebb25454d05e7389000b
Chetonix <chetonix@gmail.com>
2022-11-23 15:44:05 +0530

    add some initial code from my side to the 09-color-generator project

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,41 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+
+  const [color, setColor] = useState("#f15025");
+  const [colorList, setColorList] = useState([]);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setColorList(colors);
+      console.log(colorList);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  }
+
+  return (<>
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="#f15025" className="null" value={color} onChange={(e)=>setColor(e.target.value)}/>
+        <button className="btn" type="submit">submit</button>
+      </form>
+    </section>
+    <section className="colors">
+      {colorList.map((color, index) => {
+        return (
+          <SingleColor key={index} color={color} />
+        )
+      })}
+      
+    </section>
+    </>
+  )
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = () => {
-  return <h4>single color</h4>
+  return (
+    <article className="color false">
+        <p className="percent-value">{}</p>
+        <p className="color-value">{}</p>
+    </article>
+  )
 }
 
 export default SingleColor
