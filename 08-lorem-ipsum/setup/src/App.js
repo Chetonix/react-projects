87e9164cdabbfc2f3b9febecd2e801895df99084
Chetonix <chetonix@gmail.com>
2022-11-23 14:32:42 +0530

    add code for the 08-lorem-ipsum project, of my initial version to the repo

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -2,9 +2,42 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   
+  const [numPara, setNumPara] = useState(0);
+  const [paras, setParas] = useState([]);
+  const handleChange = (e) => {
+    setNumPara(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(numPara);
+    if (numPara <= 0) {
+      amount = 1;
+    }
+    if (numPara > 8) {
+      amount = 8;
+    }
+    setParas(data.slice(0, amount));
+    console.log(paras);
+  }
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className="section-center">
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className="lorem-form" onSubmit={handleSubmit}>
+      <label htmlFor="amount">paragraphs:</label>
+      <input type="number" name="amount" id="amount" value={numPara} onChange={handleChange}/>
+      <button className="btn">generate</button>
+    </form>
+  
+    
+        <article className="lorem-text">
+          {paras.map((para, index) => {
+            return <p key={index}>{para}</p>
+          })}
+        </article>
+    
+  </section>
     )
 }
 
