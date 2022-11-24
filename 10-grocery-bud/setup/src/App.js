10298272921c3de13a0e3451df486d0af67fcbae
Chetonix <chetonix@gmail.com>
2022-11-24 13:00:03 +0530

    debug preventDefault issue, had used wrong spell

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -7,25 +7,30 @@ function App() {
   const [name, setName] = useState("")
   const [itemList, setItemList] = useState([]);
   const [error, setError] = useState(false);
+  const [editing, isEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+
 
   const handleSubmit = (e) => {
-    e.preventdefault();
-    try {
+    e.preventDefault();
+    console.log("hello");
+    // try {
 
-      const item = {id: new Date().getTime().toString(), title: name};
-      console.log(item);
-      // setItemList(itemList.map(item => {
-      //   return item}));
+    //   const item = {id: new Date().getTime().toString(), title: name};
+    //   console.log(item);
+    //   // setItemList(itemList.map(item => {
+    //   //   return item}));
 
-      setItemList(itemList=> {
-        console.log(itemList);
-        return [...itemList, item];
-      })
-      }
-     catch (error) {
-      console.log(error);
-      setError(false);
-    }
+    //   setItemList(itemList=> {
+    //     console.log(itemList);
+    //     return [...itemList, item];
+    //   })
+    //   }
+    //  catch (error) {
+    //   console.log(error);
+    //   setError(false);
+    // }
   }
 
   return (
@@ -40,6 +45,7 @@ function App() {
       <div className='grocery-container'>
         <div className='grocery-list'>
           <List items={itemList} />
+          <button className="clear-btn" onClick={()=>setItemList([])}>clear items</button>
         </div>
       </div>
     </section>
