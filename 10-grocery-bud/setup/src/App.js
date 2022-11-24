927c4d259ec4943ccfcd89fc9a6cb6c489cd3cc0
Chetonix <chetonix@gmail.com>
2022-11-24 15:11:06 +0530

    add general code to the 10-grocery-bud project, devoid right now of localstarage call

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -7,46 +7,85 @@ function App() {
   const [name, setName] = useState("")
   const [itemList, setItemList] = useState([]);
   const [error, setError] = useState(false);
-  const [editing, isEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setItemList([]);
+  };
+
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
-    // try {
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      setItemList(
+        itemList.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
+    }
+    else {
+      showAlert(true, 'success', 'item added to the list');
+      const item = {id: new Date().getTime().toString(), title: name};
+      setItemList(itemList=> {
+      return [...itemList, item];
+      });
+      setName("");
+    }
+    
+    // console.log(item);
+    // setItemList(itemList.map(item => {
+    //   return item}));
 
-    //   const item = {id: new Date().getTime().toString(), title: name};
-    //   console.log(item);
-    //   // setItemList(itemList.map(item => {
-    //   //   return item}));
+    
+      
+     
+  }
 
-    //   setItemList(itemList=> {
-    //     console.log(itemList);
-    //     return [...itemList, item];
-    //   })
-    //   }
-    //  catch (error) {
-    //   console.log(error);
-    //   setError(false);
-    // }
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setItemList(itemList.filter(item=>item.id !== id))
+  }
+
+  const editItem = (id) => {
+    const specificItem = itemList.find(item=> item.id===id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
   }
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert {...alert} removeAlert={showAlert} list={itemList} />}
+
         <h3>grocery bud</h3>
         <div className="form-control">
           <input type="text" className="grocery" placeholder="e.g. eggs" value={name} onChange={(e)=>setName(e.target.value)}/>
-          <button type="submit" className="submit-btn">submit</button>
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
         </div>
       </form>
       <div className='grocery-container'>
-        <div className='grocery-list'>
-          <List items={itemList} />
-          <button className="clear-btn" onClick={()=>setItemList([])}>clear items</button>
-        </div>
+          <List items={itemList} removeItem={removeItem} editItem={editItem}/>
+          <button className="clear-btn" onClick={clearList}>clear items</button>
       </div>
     </section>
   )
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,36 +1,37 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items}) => {
+const List = (props) => {
 
 
   return (
       <div className='grocery-list'>
         {
-        items.map(item => {
+        props.items.map(item => {
           console.log(item);
 
-          const [id, title] = item;
+          const {id, title} = item;
 
-          return (<>
+          return (
           <article className='grocery-item' key={id}>
             <p className='title'>{title}</p>
             <div className='btn-container'>
               <button
                 type='button'
                 className='edit-btn'
-                
+                onClick = {()=>props.editItem(id)}
               >
                 <FaEdit />
               </button>
               <button
                 type='button'
                 className='delete-btn'
+                onClick={()=>props.removeItem(id)}
               >
                 <FaTrash />
               </button>
             </div>
           </article>
-          </>)
+          )
         })
         }
     </div>
